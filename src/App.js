e1f2ba9866b220ea1a19e19fea725867aaf9a29f
Brad Weldy <bradmin@Brads-MacBook-Pro.local>
2019-09-30 21:25:28 -0700

    update menu-items to navigate with Router HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
 // import logo from './logo.svg';
+import { Link, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import { tsPropertySignature } from '@babel/types';
+
+const TopicsList = props => {
+  return (
+    <div>
+      <h1>Topics List PAGE</h1>
+      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link><br />
+      <Link to={`${props.match.url}/42`}>TO TOPIC 42</Link>
+    </div>
+  );
+};
+
+const TopicDetail = props => {
+  return (
+    <div>
+      <h1>Topic Detail PAGE: {props.match.params.topicId}</h1>
+    </div>
+  );
+};
 
 function App() {
-  return <div><HomePage /></div>;
+  return (
+    <div>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/topics' component={TopicsList} />
+      <Route path='/topics/:topicId' component={TopicDetail} />
+    </div>
+  )
 }
 
 export default App;
