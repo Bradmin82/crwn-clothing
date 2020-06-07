8e639edbaf156d53b31ffc1720d6da8b68b9f704
Brad Weldy <bradmin@Brads-MacBook-Pro.local>
2020-06-07 10:25:24 -0700

    Header added and collectionPreview styles added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 // import { tsPropertySignature } from '@babel/types';
 
 const TopicsList = props => {
@@ -29,6 +30,7 @@ const TopicDetail = props => {
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
