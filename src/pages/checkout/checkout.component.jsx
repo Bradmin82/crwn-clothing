76b42ef15cc2c546065f9ff94c0615041bcc8353
Brad Weldy <bradmin@brads-mbp.lan>
2020-12-27 22:29:58 -0800

    updated cc year in checkout warning

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -37,7 +37,7 @@ const CheckoutPage = ({ cartItems, total }) => (
         <StripeCheckoutButton price={total} />
         <div className='test-warning'>*Please use the following test credit card for payments*
             <br />
-            4242 4242 4242 4242 - Exp: 1/2021 – 123
+            4242 4242 4242 4242 - Exp: 1/2022 – 123
         </div>
     </div>
 );
